970f6ad660359cc8bd49a93c328a1c7227305598
unknown <marcos.alois.oliva@gmail.com>
2022-11-08 23:31:45 -0800

    Implemting AWS login interface

--- pharmavue/src/router/index.js
+++ pharmavue/src/router/index.js
@@ -1,19 +1,61 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import { Hub } from "@aws-amplify/core"
+import Auth from "@aws-amplify/auth"
+import Login from '../components/Login.vue'
+import store from '../store/index.js'
+
+
+let user;
+
+getUser().then((user) => {
+    if (user) {
+        router.push({path: '/'});
+    }
+});
+
+function getUser() {
+    return Auth.currentAuthenticatedUser().then((data) => {
+        if (data && data.signInUserSession) {
+            store.commit('setUser', data);
+            return data;
+        }
+    }).catch(() => {
+        store.commit('setUser', null);
+        return null;
+    });
+}
+
+Hub.listen("auth", async (data) => {
+    if (data.payload.event === 'signOut'){
+        user = null;
+        store.commit('setUser', null);
+        router.push({path: '/login'});
+    } else if (data.payload.event === 'signIn') {
+        user = await getUser();
+        router.push({path: '/'});
+    }
+});
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'login',
+    component: Login
   },
+  {
+  path: '/home',
+  name: 'home',
+  component: Home
+},
   {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: { requiresAuth: true}
   }
 ]
 
@@ -22,4 +64,18 @@ const router = createRouter({
   routes
 })
 
+router.beforeResolve(async (to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        user = await getUser();
+        if (!user) {
+            return next({
+                path: '/login'
+            });
+        }
+        return next()
+    }
+    return next()
+});
+
 export default router
+
