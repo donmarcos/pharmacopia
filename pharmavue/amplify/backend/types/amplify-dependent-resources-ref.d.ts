4c54fa3700745ce057dcc15a4fafacab74d0455e
unknown <marcos.alois.oliva@gmail.com>
2022-11-13 15:24:21 -0800

    Update Amplify integration with backend

--- pharmavue/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ pharmavue/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,34 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "pharmavuec5407e92": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "pharmavueproxy": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "auth": {
+        "pharmavue": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "pharmaRest": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
