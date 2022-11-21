81374f9aafeb73d14ff534a87fb7295f512267c8
unknown <marcos.alois.oliva@gmail.com>
2022-11-20 21:11:36 -0800

    Upload module working to the AWS S3 Bucket

--- pharmavue/src/components/Upload.vue
+++ pharmavue/src/components/Upload.vue
@@ -2,135 +2,74 @@
     <div class="home">
         <img alt="Vue logo" src="../assets/pharmacopialogo.png">
     </div>
-    <div class="file-upload">
-        <div class="file-upload_area">
-            <div class="">
-                <h1 class="">Upload Your Prescription</h1>
-            </div>
-            <div>
-                <form @submit="uploadFile">
-                    <div class="">
-                        <div class="p-4">
-                            <input 
-                                type="file" 
-                                id="documentUpload" 
-                                name="image" 
-                                class="hover:cursor-pointer" 
-                                accept="image/png, image/jpeg"
-                                @change="onChange"
-                            />
-                        </div>
-                        <div class="">
-                            <input 
-                                type="submit" 
-                                value="Upload" 
-                                class="border-2 rounded-lg px-2 py-px hover:cursor-pointer"
-                            />
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-        <div 
-            class="" 
-            v-show="imageUpload"
-            @click="closeModal"
-        >
-           <br />
-            <div class="">
-                <h2 class="">Preview</h2>
-                <img :src="image" alt="Uploaded image" />
-                <button class="" @click="closeModal">
-                    Close
-                </button>
-            </div>
-        </div>
+    <div>
+        <file-pond ref="pond" :server="{
+            process: (fieldName, file, metadata, load, error, progress, abort) => {
+                uploadFile(file, metadata, load, error, progress, abort)
+            },
+        }" @removefile="onRemoveFile" />
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'Upload',
-        data() {
-            return { 
-                imageUpload: false,
-                image: ''
-            };
-        },
-        methods: {
-            onChange(e) {
-                this.image = '';
-                this.imageUpload = false;
+import vueFilePond from 'vue-filepond'
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
 
-                let files = e.target.files || e.dataTransfer.files;
+const FilePond = vueFilePond(
+    FilePondPluginFileValidateType,
+    FilePondPluginImagePreview
+);
 
-                if (!files.length) {
-                    return;
-                }
+import axios from 'axios'
+import { singleUpload, deleteObjectByKey } from './aws'
 
-                this.setImage(files[0]);
-                // this.formData.append('file', files[0]);
-            },
-            setImage(file) {
-                let reader = new FileReader();
-
-                reader.onload = (e) => {
-                    this.image = e.target.result;
-                    this.imageUpload = true;
-                    console.log(this.image);
-                }
-
-                reader.readAsDataURL(file);
-            },
-            closeModal() {
-                this.imageUpload = false;
-                /* console.log(`closeModal function called ${this.imageUpload}`) */
-            },
-            uploadFile: async function (e) {
-                e.preventDefault();
-
-                console.log("target::", e);
-
-                let formData = new FormData(e.target);
-
-                if (this.image !== '') {
-                    console.log(formData.get('image'));
-                    try {
-                        const response = await fetch(
-                            'http://localhost:4000/api/upload',
-                            {
-                                method: 'POST',
-                                body: formData
-                            }
-
-                        );
-
-                        console.log(response);
-                    } catch (error) {
-                        console.log(error.message);
-                    }
-                }
+export default {
+    name: 'Upload',
+    components: {
+        FilePond,
+    },
+    data() {
+        return {
+            imageUpload: false,
+            image: ''
+        };
+    },
+    methods: {
+        async uploadFile(file, metadata, load, error, progress, abort) {
+            const result = await singleUpload(
+                file,
+                this.$route.params.teamSlug // folder of the file, you should change it to your variable or a string
+            )
+            if (result.status === 200) {
+                // Handle storing it to your database here
+                load(file) // Let FilePond know the processing is done
+            } else {
+                error() // Let FilePond know the upload was unsuccessful
             }
-        }
+            return {
+                abort: () => {
+                    // This function is entered if the user has tapped the cancel button
+                    // Let FilePond know the request has been cancelled
+                    abort()
+                },
+            }
+        },
+        async onRemoveFile(event) {
+            let url = this.$route.params.teamSlug + '/' + event.file.name // event.file.name has only the name after the slash / so the actual filename
+            const res = await deleteObjectByKey(url)
+            if (
+                res.$response.httpResponse.statusCode >= 200 &&
+                res.$response.httpResponse.statusCode < 300
+            ) {
+                // here remove data from database too
+            }
+        },
+
     }
+}
 </script>
 
 <style scoped>
-.file-upload {
-    height: 100vh;
-    width: 100%;
-    display: flex;
-    align-items: flex-start;
-    justify-content: center;
-}
 
-.file-upload .file-upload__area {
-    width: 600px;
-    min-height: 200px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: 2px dashed #ccc;
-    margin-top: 40px;
-}
 </style>
---
+++ pharmavue/src/components/aws.js
@@ -0,0 +1,54 @@
+const aws = require('aws-sdk');
+// const axios = require('axios');
+import axios from 'axios'
+
+aws.config.update({
+  secretAccessKey: process.env.VUE_APP_AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.VUE_APP_AWS_ACCESS_KEY,
+})
+
+export const s3 = new aws.S3({
+  signatureVersion: 'v4',
+  region: process.env.VUE_APP_AWS_REGION,
+})
+
+export const singleUpload = (file, folder) => {
+  console.log(folder)
+  const key = Date.now() + '-' + file.name.replace(/\s/g, '-')
+  console.log("bucket ", process.env.VUE_APP_AWS_BUCKET)
+  console.log("file type", file.type);
+  const params = {
+    Bucket: process.env.VUE_APP_AWS_BUCKET,
+    Key: key,
+    Expires: 10,
+    ContentType: file.type,
+  }
+  const url = s3.getSignedUrl('putObject', params)
+   return axios.put(url, file, {
+      headers: {
+        'Content-Type': file.type,
+      },
+    })
+    .then(result => {
+      const bucketUrl = decodeURIComponent(result.request.responseURL).split(
+        key
+      )[0]
+      result.key = key
+      result.fullPath = bucketUrl + key
+      return result
+    })
+    .catch(err => {
+      // TODO: error handling
+      console.log(err)
+    })
+}
+
+export const deleteObjectByKey = key => {
+  const params = {
+    Bucket: process.env.VUE_APP_AWS_BUCKET,
+    Key: key,
+  }
+  const data = s3.deleteObject(params).promise()
+
+  return data
+}
